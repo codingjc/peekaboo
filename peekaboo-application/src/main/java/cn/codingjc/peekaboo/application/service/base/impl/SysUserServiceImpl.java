597fa80ad7de225d12165ce6cb204f934f94e637
codingjc <codingjc@163.com>
2023-04-25 18:09:51 +0800

    codingjc,feat:add i18n config

--- peekaboo-application/src/main/java/cn/codingjc/peekaboo/application/service/base/impl/SysUserServiceImpl.java
+++ peekaboo-application/src/main/java/cn/codingjc/peekaboo/application/service/base/impl/SysUserServiceImpl.java
@@ -1,17 +1,47 @@
 package cn.codingjc.peekaboo.application.service.base.impl;
 
 import cn.codingjc.peekaboo.application.service.base.SysUserService;
+import cn.codingjc.peekaboo.application.service.base.UserService;
 import cn.codingjc.peekaboo.domain.domain.dto.LoginRequestDTO;
+import cn.codingjc.peekaboo.domain.exception.BusinessException;
+import cn.codingjc.peekaboo.domain.exception.ErrorCodeEnum;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class SysUserServiceImpl implements SysUserService {
+
+    private final UserService service;
+
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public String login(LoginRequestDTO loginRequestDTO) {
+//        UserDetails userDetails = service.loadUserByUsername(loginRequestDTO.getUsername());
+//        if (userDetails == null) {
+//            throw new BadCredentialsException("用户名或密码不正确");
+//        }
+        Authentication authenticate = null;
+        try {
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
+            authenticate = authenticationManager.authenticate(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (AuthenticationException e) {
+            throw new BusinessException(ErrorCodeEnum.NOT_AUTHTICATION);
+        }
+        Object principal = authenticate.getPrincipal();
 
         return null;
     }
--- peekaboo-domain/src/main/java/cn/codingjc/peekaboo/domain/domain/dto/LoginRequestDTO.java
+++ peekaboo-domain/src/main/java/cn/codingjc/peekaboo/domain/domain/dto/LoginRequestDTO.java
@@ -13,6 +13,7 @@ public class LoginRequestDTO {
     @NotEmpty(message = "密码不为空")
     private String password;
 
+    @NotEmpty(message = "uuid不为空")
     private String uuid;
 
     @NotEmpty(message = "验证码不为空")
